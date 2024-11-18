443631a533334757a5c1e770d15042224d8ba5ff
Santiagoroseroq <sroseroq@unal.edu.co>
2024-11-18 11:06:23 -0500

    Update Empleado.java

--- src/gestionHumana/Empleado.java
+++ src/gestionHumana/Empleado.java
@@ -1,18 +1,21 @@
-package **;
+package gestionHumana;
 
 public class Empleado {
 
-    ** final long cedula;
-    ** String nombre;
-    ** String cargo;
+    // Atributos de la clase
+    private final long cedula;
+    private String nombre;
+    private String cargo;
     
+    // Constructor
     public Empleado(long cedula, String nombre, String cargo) {
         this.cedula = cedula;
         this.nombre = nombre;
         this.cargo = cargo;
     }
 
-    ** boolean tengoPermiso() {
+    // Método para verificar si el empleado tiene permisos
+    public boolean tengoPermiso() {
         return cargo.contains("Administrador");
     }
-}
+
